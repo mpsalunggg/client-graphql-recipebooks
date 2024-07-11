782b82315ec334ffe4ce1d584300c11f639aa0ab
mpsalunggg <putrasatria893@gmail.com>
2024-07-11 22:57:17 +0700

    feat: create input for recipe

--- src/pages/Main/components/ModalRecipe/index.tsx
+++ src/pages/Main/components/ModalRecipe/index.tsx
@@ -2,7 +2,27 @@ const ModalRecipe = () => {
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4 text-primary">Add Recipe</h2>
-      <p>Modal content goes here.</p>
+      <div className="form-control mb-4">
+        <label className="label">
+          <span className="label-text">Title</span>
+        </label>
+        <input type="text" className="input input-bordered input-sm" />
+      </div>
+      <div className="form-control mb-4">
+        <label className="label">
+          <span className="label-text">Image URL</span>
+        </label>
+        <input type="text" className="input input-bordered input-sm" />
+      </div>
+      <div className="form-control mt-4">
+        <label className="label">
+          <span className="label-text">Instructions</span>
+        </label>
+        <textarea className="textarea textarea-bordered" />
+      </div>
+      <div className="mt-4">
+        <button className="btn btn-primary btn-sm w-full">Submit</button>
+      </div>
     </div>
   )
 }
