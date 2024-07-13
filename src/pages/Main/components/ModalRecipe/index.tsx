a1420e86ded0c6983c56ae1237dfbeb9a88bdb40
mpsalunggg <putrasatria893@gmail.com>
2024-07-13 18:59:15 +0700

    feat: handle add ingredients

--- src/pages/Main/components/ModalRecipe/index.tsx
+++ src/pages/Main/components/ModalRecipe/index.tsx
@@ -1,4 +1,29 @@
+import { ChangeEvent, useState } from 'react'
+import { FaMinusCircle } from 'react-icons/fa'
+
 const ModalRecipe = () => {
+  const [ingredients, setIngredients] = useState<string[]>([])
+  const [recipe, setRecipe] = useState({
+    title: '',
+    image_url: '',
+    ingredients: '',
+    instructions: '',
+  })
+
+  const handleAddIngredients = (value: string) => {
+    console.log(recipe)
+    setIngredients([...ingredients, value])
+  }
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target
+    setRecipe({
+      ...recipe,
+      [name]: value,
+    })
+  }
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4 text-primary">Add Recipe</h2>
@@ -6,13 +31,23 @@ const ModalRecipe = () => {
         <label className="label">
           <span className="label-text">Title</span>
         </label>
-        <input type="text" className="input input-bordered input-sm" />
+        <input
+          name="title"
+          type="text"
+          className="input input-bordered input-sm"
+          onChange={handleChange}
+        />
       </div>
       <div className="form-control mb-4">
         <label className="label">
           <span className="label-text">Image URL</span>
         </label>
-        <input type="text" className="input input-bordered input-sm" />
+        <input
+          name="image_url"
+          type="text"
+          className="input input-bordered input-sm"
+          onChange={handleChange}
+        />
       </div>
       <div>
         <div className="form-control mt-4">
@@ -21,44 +56,42 @@ const ModalRecipe = () => {
           </label>
           <div className="flex items-center gap-2">
             <input
+              name="ingredients"
               type="text"
               className="input input-bordered input-sm w-full"
+              onChange={handleChange}
             />
-            <button className="btn btn-primary btn-sm">+</button>
+            <button
+              className="btn btn-primary btn-sm"
+              onClick={() => {
+                handleAddIngredients(recipe.ingredients)
+              }}
+            >
+              +
+            </button>
           </div>
         </div>
         <div className="mt-4 flex flex-wrap">
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomat
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
-          <div className="badge badge-primary badge-outline rounded-sm">
-            Tomato
-          </div>
+          {ingredients.map((item, index) => (
+            <div
+              key={index}
+              className="badge badge-primary badge-outline rounded-sm flex items-center gap-1"
+            >
+              {item}
+              <FaMinusCircle size={12} />
+            </div>
+          ))}
         </div>
       </div>
       <div className="form-control mt-4">
         <label className="label">
           <span className="label-text">Instructions</span>
         </label>
-        <textarea className="textarea textarea-bordered" />
+        <textarea
+          name="instructions"
+          className="textarea textarea-bordered"
+          onChange={handleChange}
+        />
       </div>
       <div className="mt-4">
         <button className="btn btn-primary btn-sm w-full">Submit</button>
